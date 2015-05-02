4117d762277da40b537bb67b63d665388f12decb
Sergey <serwaas@yandex.ru>
2015-05-02 10:46:02 +0300

    FluentAPI

--- FluentApi/Program.cs
+++ FluentApi/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace FluentTask
 {
@@ -7,24 +9,80 @@ namespace FluentTask
 		private static void Main()
 		{
 
-/*
-			var behaviour = new Behavior()
-				.Say("Привет мир!")
-				.UntilKeyPressed(b => b
-					.Say("Ля-ля-ля!")
-					.Say("Тру-лю-лю"))
-				.Jump(JumpHeight.High)
-				.UntilKeyPressed(b => b
-					.Say("Aa-a-a-a-aaaaaa!!!")
-					.Say("[набирает воздух в легкие]"))
-				.Say("Ой!")
-				.Delay(TimeSpan.FromSeconds(1))
-				.Say("Кто здесь?!")
-				.Delay(TimeSpan.FromMilliseconds(2000));
 
-			behaviour.Execute();
-*/
+		    var behaviour = new Behavior()
+		        .Say("Привет мир!")
+            				.UntilKeyPressed(b => b
+                                .Say("Ля-ля-ля!")
+                                .Say("Тру-лю-лю"))
+                            .Jump(JumpHeight.High)
+                            .UntilKeyPressed(b => b
+                                .Say("Aa-a-a-a-aaaaaa!!!")
+                                .Say("[набирает воздух в легкие]"))
+                            .Say("Ой!")
+                           .Delay(TimeSpan.FromSeconds(1))
+                            .Say("Кто здесь?!")
+                            .Delay(TimeSpan.FromMilliseconds(2000));
+            
+            behaviour.Execute();
+
 
 		}
 	}
+
+    public class Behavior
+    {
+        private List<Action<Behavior>> _actions = new List<Action<Behavior>>();
+
+        public Behavior Say(string something)
+        {
+            _actions.Add(behavior => Console.WriteLine(something));
+            return this;
+        }
+
+        public Behavior UntilKeyPressed(Action<Behavior> action)
+        {
+            _actions.Add(b =>
+            {
+                var behavior = new Behavior();
+                while (!Console.KeyAvailable)
+                {
+                    action.Invoke(behavior);
+                    behavior.Execute();
+                    Thread.Sleep(500);
+                }
+                Console.ReadKey(true);
+            });
+            return this;
+        }
+
+        public Behavior Jump(JumpHeight height)
+        {
+            _actions.Add(behavior =>
+            {
+                var line = height == JumpHeight.High ? "Высоко прыгнул" : "Низко прыгнул";
+                Console.WriteLine(line);
+            });
+            return this;
+        }
+
+        public Behavior Delay(TimeSpan time)
+        {
+            _actions.Add(behavior =>
+            {
+                Thread.Sleep(time);
+            });
+            return this;
+        }
+
+        public void Execute()
+        {
+            foreach (var action in _actions)
+            {
+                action.Invoke(this);
+            }
+            _actions.Clear();
+        }
+
+    }
 }
